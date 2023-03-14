5e728c12e978491544bda6ebfc9782c7bf0389c8
Juraj <118258305+allomik69@users.noreply.github.com>
2023-03-14 23:10:33 +0100

    Add files via upload

---
+++ 2023 lvl2 php/function.php
@@ -0,0 +1,40 @@
+<?php
+date_default_timezone_set("Asia/Tokyo");
+if (file_exists("studenti.json"))
+      {
+        $encodedStudents= file_get_contents("studenti.json");
+        $students=  !empty($encodedStudents) ? json_decode( $encodedStudents, true) : array();
+        $name = $_GET["meno"]; 
+        if (array_key_exists($name, $students)) 
+        {
+        $students[$name]++;
+        } 
+        else 
+        {
+        $students[$name] = 1;
+        }   
+    $json = json_encode($students, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+    file_put_contents('studenti.json', $json);
+    } 
+function verification($H,$cas_text,$cas) 
+    {   
+        if (8<$H &&  $H<20)  
+        {
+
+            file_put_contents($cas_text,$cas. " meskanie" ." ".$_GET["meno"]."<br>", FILE_APPEND);
+        } 
+        
+        else if (20 <=  $H && 23 >=  $H) 
+        {
+            die("chyba");
+        } 
+        else 
+        {
+
+            file_put_contents($cas_text, $cas. " ". $_GET["meno"] ."<br>", FILE_APPEND);
+        }
+        header("Location: index.php");
+        die();
+    }
+    
+verification(date("H"),"cas.txt",date("H.i.s"));

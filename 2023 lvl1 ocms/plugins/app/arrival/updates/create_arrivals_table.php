5efbb42b81866579b7b133fb844065885ff83b70
Juraj <118258305+allomik69@users.noreply.github.com>
2023-04-17 21:35:04 +0200

    pridanie user_id

--- 2023 lvl1 ocms/plugins/app/arrival/updates/create_arrivals_table.php
+++ 2023 lvl1 ocms/plugins/app/arrival/updates/create_arrivals_table.php
@@ -13,6 +13,7 @@ class CreateArrivalsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->timestamp('arrival');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }

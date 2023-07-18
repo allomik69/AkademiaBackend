a2b69bce1d3d8cfee98505d039b705a66a7f383d
Juraj <118258305+allomik69@users.noreply.github.com>
2023-07-18 02:35:45 +0200

    feedback

--- 2023lvl4ocms/plugins/teamgrid/task/models/Task.php
+++ 2023lvl4ocms/plugins/teamgrid/task/models/Task.php
@@ -17,27 +17,21 @@ class Task extends Model
     ];
     public $hasMany = [
         'timeEntries' => ['Teamgrid\Timeentry\Models\TimeEntry'],
-    ];
+    ]; 
+
     public function getTrackedTimeAttribute()
     {
         $task_time_entries = TimeEntry::where('task_id', $this->id)->get();
-
-        $total_times = [];
+        $tracked_time = CarbonInterval::create();       
         foreach ($task_time_entries as $time_entry) 
         {
-            $total_times[] = $time_entry->total_time;
-        }
-        $tracked_time = CarbonInterval::create();
-    
-        foreach ($total_times as $one_total_time) 
-        {
-            $interval = CarbonInterval::createFromDateString($one_total_time);
+            $interval = CarbonInterval::createFromDateString($time_entry->total_time);
             $tracked_time = $tracked_time->add($interval);
         }
-        $tracked_time = $tracked_time->subYears(1);
-        
-        return $tracked_time->cascade()->format('%y years, %m months, %d days, %H hours, %i minutes, %s seconds');
+        $tracked_time = $tracked_time->subYears(1);      
+        return $tracked_time;
     }
+    
     /**
      * @var string The database table used by the model.
      */
